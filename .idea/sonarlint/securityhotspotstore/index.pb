003b87f6d54e1cc7fddf2031834cfa52fcdca349
Jose Ayala <jayala@starbucks.com>
2024-06-01 18:43:11 -0500

    Add task02 with a url lambda fnction

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+`
+0task02/jsrc/main/java/com/task02/HelloWorld.java,7/f/7f946ed7a55bacfad358955a4e76ab63e3c13121
+>
+task02/pom.xml,2/0/207cc21099f7c9ae93633cef03b2a12cce086982
---
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -0,0 +1,39 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "hello_world",
+	roleName = "hello_world-role",
+	isPublishVersion = false,
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+        String path = request.getPath();
+        String method = request.getHttpMethod();
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        response.setHeaders(headers);
+
+        if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
+            response.setStatusCode(200);
+            response.setBody("{\"message\": \"Hello from Lambda\"}");
+        } else {
+            response.setStatusCode(400);
+            response.setBody(String.format("{\"message\": \"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s\"}", path, method));
+        }
+        return response;
+    }
+}
